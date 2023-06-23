e8aec8691cc2dd9297c93456af2deeeeb8dc473f
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-06-23 13:23:35 -0500

    UsuarioRepository

---
+++ src/main/java/com/example/DAWI_CL3_Grupo3/repository/UsuarioRepository_CL3.java
@@ -0,0 +1,15 @@
+package com.example.DAWI_CL3_Grupo3.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.DAWI_CL3_Grupo3.model.bd.Usuario;
+
+@Repository
+public interface UsuarioRepository_CL3 extends JpaRepository<Usuario, Integer>{
+	
+	Usuario findByEmail(String email_cl3);
+	
+	Usuario findByNomusuario(String nomusu_cl3);
+
+}
