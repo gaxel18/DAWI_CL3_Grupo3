92961debfabc3199bfd5876bd9dad4beb4cbf42c
valeriadiaz007 <valeriadiaz.guardamino0712@gmail.com>
2023-06-23 12:49:51 -0500

    Valeria Diaz Guardamino 1

---
+++ src/main/java/com/example/DAWI_CL3_Grupo3/model/bd/Rol.java
@@ -0,0 +1,28 @@
+package com.example.DAWI_CL3_Grupo3.model.bd;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="rol")
+public class Rol {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idrol; 
+	@Column(name="nomrol")
+	private String nomrol; 
+
+}
