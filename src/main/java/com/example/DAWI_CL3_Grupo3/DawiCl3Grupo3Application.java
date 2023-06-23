34b287187115a2069f1c790ca77ecc32a99a08b9
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-06-23 11:08:57 -0500

    Inicio

---
+++ src/main/java/com/example/DAWI_CL3_Grupo3/DawiCl3Grupo3Application.java
@@ -0,0 +1,13 @@
+package com.example.DAWI_CL3_Grupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3Grupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3Grupo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DAWI_CL3_Grupo3/DawiCl3Grupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DAWI_CL3_Grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3Grupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
